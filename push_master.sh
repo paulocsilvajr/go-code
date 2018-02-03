437e5b3176a65ed1540fb332474130067a643e9b
paulocsilvajr <pauluscave@gmail.com>
2018-02-03 09:08:08 -0200

    Alterado e melhorado push_master

--- push_master.sh
+++ push_master.sh
@@ -1,14 +1,37 @@
 #!/bin/bash
 
-echo 'Atualizando master com as alterações de desenvolvimento'
-git rebase desenvolvimento master
+branch_atual() {
+    echo $(git branch | grep '*' | awk -F ' +' '! /\(no branch\)/ {print $2}')
+}
 
-echo
-git branch
-echo
+BRANCH_ATUAL=$(branch_atual)
+DEV='desenvolvimento'
+MASTER='master'
 
-echo 'Enviando alterações para origin'
-git push origin master
+if [ $BRANCH_ATUAL == $DEV ]; then
+    clear
+    echo 'Atualizando master com as alterações de desenvolvimento'
+    git rebase desenvolvimento master
 
-echo 'Alterado branch de master para desenvolvimento'
-git checkout desenvolvimento
+    BRANCH_ATUAL=$(branch_atual)
+
+    if [ $BRANCH_ATUAL == $MASTER ]; then
+        echo
+        git branch
+        echo
+
+        echo 'Enviando alterações para origin'
+        git push origin master
+
+        echo
+        echo 'Alterado branch de master para desenvolvimento'
+        git checkout desenvolvimento
+
+    else
+        echo -e '\nAdicione(add) e grave(commit) as alterações de' $DEV '\n'
+    fi
+
+else
+    echo -e '\nAltere para o branch' $DEV '\n'
+fi
+
