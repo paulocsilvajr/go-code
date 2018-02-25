f7e11fef4fe94191797d1a4992fa069904cf99de
paulocsilvajr <pauluscave@gmail.com>
2018-02-25 18:39:16 -0300

    Melhorado run.sh

--- server_restful_json/run.sh
+++ server_restful_json/run.sh
@@ -1,3 +1,16 @@
 #!/bin/bash
 
-go run *.go
+if [ "$1" == "-h" ]; then
+    echo "Sintaxe: $0 [ -t | -j ]"
+    echo "         -t testes(go run)"
+    echo "         -h Ajuda"
+    echo "         Quando omite-se o parâmetro, faz o build(go build) e executa"
+
+elif [ "$1" == "-t" ]; then
+    go run *.go
+
+else
+    ./build.sh
+    ./server_restful_json
+
+fi
