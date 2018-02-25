e6313f338f104851a47ec1336f0be80630162e0d
paulocsilvajr <pauluscave@gmail.com>
2018-02-25 12:07:33 -0300

    Better Model(json)

--- server_restful_json/person/person.go
+++ server_restful_json/person/person.go
@@ -3,9 +3,9 @@ package person
 import "time"
 
 type Person struct {
-	Name      string
-	Active    bool
-	Create_at time.Time
+	Name      string    `json:"name"`
+	Active    bool      `json:"active"`
+	Create_at time.Time `json:"create_at"`
 }
 
 type Persons []Person
