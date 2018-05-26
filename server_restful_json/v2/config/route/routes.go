98d3b4d10188b8ffb5eae602731f8226815a7a25
paulocsilvajr <pauluscave@gmail.com>
2018-05-26 16:24:01 -0300

    Alterado log para redirecionar também em arquivo

--- server_restful_json/v2/config/route/routes.go
+++ server_restful_json/v2/config/route/routes.go
@@ -14,12 +14,6 @@ type Route struct {
 
 type Routes []Route
 
-// rotas:
-// [GET]  localhost:8080           handlers.Index        -> indice(welcome)
-// [GET]  localhost:8080/persons   handlers.PersonIndex  -> lista pessoas em array de jsons
-// [GET]  localhost:8080/persons/1 handlers.PersonShow   -> pessoa com id 1 em json
-// [POST] localhost:8080/persons   handlers.PersonCreate -> cadastro de pessoa via json
-
 var routes = Routes{
 	Route{
 		"Index",
--- server_restful_json/v2/logger/logger.go
+++ server_restful_json/v2/logger/logger.go
@@ -1,8 +1,11 @@
 package logger
 
 import (
+	"fmt"
+	"io"
 	"log"
 	"net/http"
+	"os"
 	"time"
 )
 
@@ -11,6 +14,18 @@ func Logger(inner http.Handler, name string) http.Handler {
 		start := time.Now()
 
 		inner.ServeHTTP(w, r)
+
+		// log em arquivo
+		nomeArquivo := fmt.Sprintf("logger/logs/%04d%02d%02d.log", start.Year(), start.Month(), start.Day())
+		arquivo, err := os.OpenFile(nomeArquivo, os.O_RDWR|os.O_CREATE|os.O_APPEND, 0666)
+		if err != nil {
+			log.Fatalf("Erro ao abrir arquivo de log[%s]", err)
+		}
+		defer arquivo.Close()
+
+		// log em arquivo(arquivo) e tela(Stdout)
+		multiplaSaida := io.MultiWriter(os.Stdout, arquivo)
+		log.SetOutput(multiplaSaida)
 
 		log.Printf(
 			"%s\t%s\t%s\t%s",
