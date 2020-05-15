23201398d96020a3709a86803237463514e63ebe
Paulo Carvalho da Silva Junior <pauluscave@gmail.com>
2020-05-15 09:07:19 -0300

    Renomeado Dir para DirM. Criado função DirF para retornar atributos. Criado função Dir para retornar métodos e atributos em um map. Criado teste unitário para as novas funções. Adicionado Docstring em funções

--- util/functions.go
+++ util/functions.go
@@ -4,9 +4,9 @@ import (
 	"reflect"
 )
 
-// Dir retorna um slice de strings contendo os métodos Públicos da classe informada
-func Dir(v interface{}) []string {
-	tipo := reflect.TypeOf(v)
+// DirM retorna um slice de strings contendo os métodos Públicos da struct informada. Retorna um slice vazio se a variável informada não for uma struct
+func DirM(v interface{}) []string {
+	tipo := getType(v)
 
 	var metodos []string
 	for i := 0; i < tipo.NumMethod(); i++ {
@@ -17,9 +17,43 @@ func Dir(v interface{}) []string {
 	return metodos
 }
 
+// DirF retorna um slice de strings contendo os atributos da struct informada. Retorna um slice vazio se a variável informada não for uma struct
+func DirF(v interface{}) []string {
+	tipo := getType(v)
+
+	if tipo.Kind() != reflect.Struct {
+		return []string{}
+	}
+
+	var atributos []string
+	for i := 0; i < tipo.NumField(); i++ {
+		a := tipo.Field(i)
+		atributos = append(atributos, a.Name)
+	}
+
+	return atributos
+}
+
+// Dir retorna um map contendo os métodos(chave: methods) e atributos(chave: fields) da struct informada.
+func Dir(v interface{}) map[string][]string {
+	metodos := DirM(v)
+	atributos := DirF(v)
+
+	dir := make(map[string][]string)
+	dir["methods"] = metodos
+	dir["fields"] = atributos
+
+	return dir
+}
+
 // Type retorna uma string contendo o tipo da variável informada
 func Type(v interface{}) string {
-	t := reflect.TypeOf(v)
+	t := getType(v)
 
 	return t.Name()
+}
+
+// getType retorna o tipo(reflect.Type) da variável passada como parâmetro
+func getType(v interface{}) reflect.Type {
+	return reflect.TypeOf(v)
 }
--- util/functions_test.go
+++ util/functions_test.go
@@ -35,16 +35,36 @@ func TestType(t *testing.T) {
 
 func TestDir(t *testing.T) {
 	p := Pessoa{"José", 30}
-	metodos := Dir(p)
+	metodos := DirM(p)
 	if len(metodos) > 2 {
-		t.Error("Dir retornou um slice com mais de 2 elemento. Struct Pessoa tem somente 2 método Públicos", metodos)
+		t.Error("DirM retornou um slice com mais de 2 elemento. Struct Pessoa tem somente 2 método Públicos", metodos)
 	}
 
 	if metodos[0] != "Andar" {
-		t.Error("Dir não retornou o método declarado na struct Pessoa chamado Andar")
+		t.Error("DirM não retornou o método declarado na struct Pessoa chamado Andar")
 	}
 
 	if metodos[1] != "Correr" {
-		t.Error("Dir não retornou o método declarado na struct Pessoa chamado Correr")
+		t.Error("DirM não retornou o método declarado na struct Pessoa chamado Correr")
+	}
+
+	atributos := DirF(p)
+	if len(atributos) > 2 {
+		t.Error("DirF retornou um slice com mais de 2 elementos. Struct Pessoa tem somente 2 atributos", atributos)
+	}
+
+	metodos = DirM(42)
+	if len(metodos) > 0 {
+		t.Error("DirM retornou métodos para uma variável inteira", metodos)
+	}
+
+	atributos = DirF(42)
+	if len(atributos) > 0 {
+		t.Error("DirF retornou atributos para uma variável inteira", atributos)
+	}
+
+	dir := Dir(p)
+	if len(dir["methods"]) > 2 && len(dir["fields"]) > 2 {
+		t.Error("Dir retornou mais de 2 métodos e 2 atributos. Struct Pessoa tem 2 métodos publicos e 2 atributos", dir)
 	}
 }
